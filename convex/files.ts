a569b3f37d9291f18fa4145b9240558d534eb290
musiienko25 <devmusiienko@gmail.com>
2024-03-06 15:58:24 +0200

    added createfile and mapfile into convex

---
+++ convex/files.ts
@@ -0,0 +1,20 @@
+import { mutation, query } from "./_generated/server";
+import { v } from "convex/values";
+
+export const createFile = mutation({
+    args: { 
+        name: v.string()
+    },
+    async handler(ctx, args) {
+      await ctx.db.insert("files", {
+        name: args.name
+      })
+    }
+});
+
+export const getFiles = query({
+  args: {},
+  async handler(ctx, args) {
+      return ctx.db.query('files').collect()
+  }
+})
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import {
@@ -6,8 +8,12 @@ import {
   SignedOut,
   SignedIn,
 } from "@clerk/nextjs";
+import { api } from "../../convex/_generated/api";
+import { useMutation, useQuery } from "convex/react";
 
 export default function Home() {
+  const createFile = useMutation(api.files.createFile);
+  const files = useQuery(api.files.getFiles);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <SignedOut>
@@ -20,7 +26,16 @@ export default function Home() {
         <SignOutButton></SignOutButton>
       </SignedIn>
 
-      <Button>Button</Button>
+      {files?.map((file) => {
+        return <div key={file._id}>{file.name}</div>;
+      })}
+      <Button
+        onClick={() => {
+          createFile({ name: "hello world" });
+        }}
+      >
+        Button
+      </Button>
     </main>
   );
 }
