ad96efdfb8914fb1bf28697aa11511080603026e
musiienko25 <devmusiienko@gmail.com>
2024-03-06 13:02:24 +0200

    move middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { SignInButton } from "@clerk/nextjs";
+import {
+  SignInButton,
+  SignOutButton,
+  SignedOut,
+  SignedIn,
+} from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SignInButton mode="modal"></SignInButton>
-      {/* <SignOutButton></SignOutButton> */}
+      <SignedOut>
+        {" "}
+        <SignInButton mode="modal"></SignInButton>{" "}
+      </SignedOut>
+
+      <SignedIn>
+        {" "}
+        <SignOutButton></SignOutButton>
+      </SignedIn>
+
       <Button>Button</Button>
     </main>
   );
